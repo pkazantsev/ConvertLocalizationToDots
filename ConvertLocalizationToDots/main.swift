77898a7778529641de2020082db63f55ff48f752
Pavel Kazantsev <p.kazantsev@tinkoff.ru>
2017-10-24 10:13:27 +0300

    Initial Commit

---
+++ ConvertLocalizationToDots/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ConvertLocalizationToDots
+//
+//  Created by Pavel Kazantsev on 24/10/2017.
+//  Copyright © 2017 Pavel Kazantsev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
