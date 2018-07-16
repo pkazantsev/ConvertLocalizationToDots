14b280634d7c231a7e148eb98af3bbbdf2553748
Pavel Kazantsev <p.kazantsev@tinkoff.ru>
2018-07-16 15:01:27 +0300

    Added reading filters from the config file

--- ConvertLocalizationToDots/Config.swift
+++ ConvertLocalizationToDots/Config.swift
@@ -39,7 +39,7 @@ class Config {
             if aLine.starts(with: "//") {
                 // Comment, ignore
             }
-            else if aLine.starts(with: "  ") {
+            else if aLine.starts(with: "  ") || aLine.starts(with: "\t") {
                 switch currentGroup {
                 case .unknown:
                     break
@@ -69,8 +69,81 @@ class Config {
         }
     }
 
+    // MARK: - Filters
+
+    fileprivate enum FilterOperation: String {
+        case notEmpty = "!empty"
+        case notEqual = "!eq"
+        case notPrefix = "!prefix"
+        case notSuffix = "!suffix"
+        case notContains = "!contains"
+    }
+
+    fileprivate enum FilterKey: String {
+        case key
+        case comment
+    }
+
     private func parseFilter(from string: String) -> RowFilter? {
-        // FIXME: parseFilter(from:) not implemented!
-        return nil
+        // 1. find the value
+        let valueStartIndex = string.index(of: "\"") ?? string.endIndex
+
+        // 2. take the rest and find the column and the operator
+        let ops = string[..<valueStartIndex].split(separator: " ")
+        guard ops.count == 2 else { return nil }
+
+        guard let key = FilterKey(rawValue: String(ops[0])) else { return nil }
+        guard let op = FilterOperation(rawValue: String(ops[1])) else { return nil }
+
+        // 3. get the value
+        let value = string[valueStartIndex...].trimmingCharacters(in: .init(charactersIn: "\""))
+
+        // 4. create a function
+        switch op {
+        case .notEmpty: return isNotEmpty(key: key)
+        case .notEqual: return isNotEqual(key: key, value: value)
+        case .notPrefix: return dontHavePrefix(key: key, value: value)
+        case .notSuffix: return dontHaveSuffix(key: key, value: value)
+        case .notContains: return doesNotContain(key: key, value: value)
+        }
+    }
+
+}
+
+private func value(for key: Config.FilterKey, from row: Row) -> String? {
+    switch key {
+    case .key: return row.key
+    case .comment: return row.comment
+    }
+}
+
+private func isNotEmpty(key: Config.FilterKey) -> RowFilter {
+    return { row in
+        guard let testStr = value(for: key, from: row) else { return false }
+        return !testStr.isEmpty
+    }
+}
+private func isNotEqual(key: Config.FilterKey, value str: String) -> RowFilter {
+    return { row in
+        guard let testStr = value(for: key, from: row) else { return true }
+        return testStr != str
+    }
+}
+private func dontHavePrefix(key: Config.FilterKey, value str: String) -> RowFilter {
+    return { row in
+        guard let testStr = value(for: key, from: row) else { return true }
+        return !testStr.hasPrefix(str)
+    }
+}
+private func dontHaveSuffix(key: Config.FilterKey, value str: String) -> RowFilter {
+    return { row in
+        guard let testStr = value(for: key, from: row) else { return true }
+        return !testStr.hasSuffix(str)
+    }
+}
+private func doesNotContain(key: Config.FilterKey, value str: String) -> RowFilter {
+    return { row in
+        guard let testStr = value(for: key, from: row) else { return true }
+        return !testStr.contains(str)
     }
 }
